1306f7abe125fea3536e67db5cf03bfb26089c8c
guilhermecoelho2@gmail.com <guilhermecoelho2@gmail.com@c2fb1b3e-acd2-b25d-0dd2-99df1911dfd4>
2012-08-16 16:59:39 +0000

    criação consulta_cliente.xhtml e das classes listaCliente em clienteControler.java e clienteDAO.java

--- Fideliza/src/br/com/fideliza/controller/ClienteController.java
+++ Fideliza/src/br/com/fideliza/controller/ClienteController.java
@@ -1,13 +1,16 @@
 package br.com.fideliza.controller;
 
+import java.util.List;
+
 import javax.faces.application.FacesMessage;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.RequestScoped;
 import javax.faces.context.FacesContext;
+import javax.faces.model.DataModel;
+import javax.faces.model.ListDataModel;
 
 import br.com.fideliza.DAO.ClienteDAO;
 import br.com.fideliza.model.Cliente;
-
 
 @ManagedBean(name = "ClienteController")
 @RequestScoped
@@ -17,30 +20,47 @@ public class ClienteController {
 	private ClienteDAO clienteDAO;
 	private boolean flag = false;
 
+	private DataModel<Cliente> clienteLista;
+
 	public ClienteController() {
-		
+
 		this.cliente = new Cliente();
 		this.clienteDAO = new ClienteDAO();
 	}
 
 	public String salvaCliente() {
-		if(verificaEmail() == true){
+		if (verificaEmail() == true) { // se o email está certo salva cadastro
 			flag = true;
 			clienteDAO.adicionaCliente(cliente);
 			return "save";
 		} else {
 			return "erro";
 		}
-		
+
 	}
-	
-	public boolean verificaEmail(){
-		if(!cliente.getEmail().equals(cliente.getEmailConfirm())){
-			FacesContext.getCurrentInstance().addMessage(null, new FacesMessage(FacesMessage.SEVERITY_ERROR, "email invalido", null));
+
+	public boolean verificaEmail() { // verifica se os campos "email" e  "confirma email" são iguais
+										
+		if (!cliente.getEmail().equals(cliente.getEmailConfirm())) {
+			FacesContext.getCurrentInstance().addMessage(
+					null,
+					new FacesMessage(FacesMessage.SEVERITY_ERROR,
+							"email invalido", null));
 			return false;
 		} else {
 			return true;
 		}
+	}
+
+	public DataModel<Cliente> getClienteLista() { // lista todos clientes
+		List<Cliente> cliente = new ClienteDAO().listaClientes();
+		clienteLista = new ListDataModel<Cliente>(cliente);
+		return clienteLista;
+
+	}
+
+	public void setClienteLista(DataModel<Cliente> clienteLista) {
+		this.clienteLista = clienteLista;
 	}
 
 	public Cliente getcliente() {
@@ -66,7 +86,5 @@ public class ClienteController {
 	public void setFlag(boolean flag) {
 		this.flag = flag;
 	}
-	
-	
 
 }
