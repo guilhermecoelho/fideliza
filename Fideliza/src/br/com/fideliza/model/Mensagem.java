c19e39694e28bad880d49df17794e3656220e38b
guilhermecoelho2@gmail.com <guilhermecoelho2@gmail.com@c2fb1b3e-acd2-b25d-0dd2-99df1911dfd4>
2012-11-16 16:00:19 +0000

    incluido serviço de emails

---
+++ Fideliza/src/br/com/fideliza/model/Mensagem.java
@@ -0,0 +1,30 @@
+package br.com.fideliza.model;
+
+public class Mensagem {
+	
+	private String destino;
+	private String mensagem;
+	private String titulo;
+	
+	public String getDestino() {
+		return destino;
+	}
+	public void setDestino(String destino) {
+		this.destino = destino;
+	}
+	public String getMensagem() {
+		return mensagem;
+	}
+	public void setMensagem(String mensagem) {
+		this.mensagem = mensagem;
+	}
+	public String getTitulo() {
+		return titulo;
+	}
+	public void setTitulo(String titulo) {
+		this.titulo = titulo;
+	}
+	
+	
+
+}
